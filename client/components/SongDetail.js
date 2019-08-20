6fa87e4b1db57f322698dd8e57a9f754461b8c7d
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-08-20 19:04:43 +0300

    final commit

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -15,6 +16,7 @@ class SongDetail extends Component {
       <div>
         <Link to='/'>Back</Link>
         <h4>{song.title}</h4>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
